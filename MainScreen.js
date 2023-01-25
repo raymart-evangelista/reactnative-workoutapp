974b9591ec2aa3fcf2c37ba5e5d418a1ffb23a7b
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-01-24 22:46:20 -0800

    add own content

--- MainScreen.js
+++ MainScreen.js
@@ -29,18 +29,27 @@ export default class MainScreen extends Component {
       <View style={styles.container}>
         <View style={styles.dashboardContainer}>
           <Text style={styles.title}>{this.state.greeting}, Raymart.</Text>
-          <Text style={styles.subtitle}>Recent Workouts</Text>
-          <Text style={styles.workoutData}>Workouts Completed: 5</Text>
+          <Text style={styles.title}>Your programs</Text>
+          {/* <Text style={styles.subtitle}>Recent Workouts</Text> */}
+          {/* <Text style={styles.workoutData}>Workouts Completed: 5</Text>
           <Text style={styles.workoutData}>Total Distance: 10km</Text>
-          <Text style={styles.workoutData}>Total Calories Burned: 500</Text>
+          <Text style={styles.workoutData}>Total Calories Burned: 500</Text> */}
         </View>
 
-        <TouchableOpacity 
-            style={styles.startWorkoutButton}
-            onPress={() => this.props.navigation.navigate('NewWorkout')}
+        <TouchableOpacity
+          style={styles.startWorkoutButton}
+          onPress={() => this.props.navigation.navigate('NewProgram')}
+        >
+          <Text style={styles.startWorkoutButtonText}>Create new program</Text>
+
+        </TouchableOpacity>
+
+        {/* <TouchableOpacity 
+          style={styles.startWorkoutButton}
+          onPress={() => this.props.navigation.navigate('NewWorkout')}
         >
             <Text style={styles.startWorkoutButtonText}>Start Workout</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     );
   }
